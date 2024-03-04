628e5ac6451238fa870764336a0a4441026f9f6f
Facundo <facundodenis7@gmail.com>
2024-03-04 13:43:07 -0300

    ultimas_modificaciones

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pyarrow as pa
 import pyarrow.parquet as pq
 from fastapi import FastAPI
 from sklearn.metrics.pairwise import cosine_similarity
-
+from fastapi.responses import HTMLResponse
 
 
 app = FastAPI()
@@ -16,6 +16,57 @@ df_UserForGenre_parte_2 = pd.read_parquet("data/user_for_genre_part_2.parquet")
 df_UsersRecommend = pd.read_parquet("data/UsersRecommend.parquet")
 df_UsersWorstDeveloper = pd.read_parquet("data/UsersWorstDeveloper.parquet")
 df_Sentiment_Analysis = pd.read_parquet("data/sentiment_analysis.parquet")
+
+
+@app.get(path="/", response_class=HTMLResponse, tags=["Home"])
+def presentacion():
+    '''
+    Genera una página de presentación HTML para la API Steam de consultas de videojuegos.
+    
+    Returns:
+    str: Código HTML que muestra la página de presentación.
+    '''
+    return '''
+    <html>
+        <head>
+            <title>API Steam</title>
+            <style>
+                body {
+                    font-family: Arial, sans-serif;
+                    padding: 20px;
+                }
+                h1 {
+                    color: #333;
+                    text-align: center;
+                }
+                p {
+                    color: #666;
+                    text-align: center;
+                    font-size: 18px;
+                    margin-top: 20px;
+                }
+            </style>
+        </head>
+        <body>
+            <h1>API de consultas de videojuegos de la plataforma Steam</h1>
+            <p>Bienvenido a la API de Steam donde se pueden hacer diferentes consultas sobre la plataforma de videojuegos.</p>
+            <p>INSTRUCCIONES:</p>
+            <p>Escriba <span style="background-color: lightgray;">/docs</span> a continuación de la URL actual de esta página para interactuar con la API</p>
+            <p> Visita mi perfil en <a href="https://www.linkedin.com/in/ingambcarlapezzone/"><img alt="LinkedIn" src="https://img.shields.io/badge/LinkedIn-blue?style=flat-square&logo=linkedin"></a></p>
+            <p> El desarrollo de este proyecto esta en <a href="https://github.com/IngCarlaPezzone/PI1_MLOps_videojuegos"><img alt="GitHub" src="https://img.shields.io/badge/GitHub-black?style=flat-square&logo=github"></a></p>
+        </body>
+    </html>
+    '''
+@app.get(
+    path="/developer",
+    description=""" <font color="green">
+                        INSTRUCCIONES<br>
+                        
+Haga clik en "Try it out".<br>
+Ingrese el Desarrollador en la caja de abajo.<br>
+Scrollear a "Resposes" para ver la cantidad de items y porcentaje de contenido Free por año según empresa de desarrollo.</font>""",
+tags=["Consultas Generales"],
+)
 
 
 # Primera funcion: PlaytimeGenre
